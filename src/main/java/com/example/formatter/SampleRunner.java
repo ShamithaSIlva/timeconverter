3eb44eb68c1b0df24b9504784fd04c99aff4c93a
ShamithaSilva <shamithadevinda@gmail.com>
2019-11-24 19:23:45 +0000

    main stub

---
+++ src/main/java/com/example/formatter/SampleRunner.java
@@ -0,0 +1,12 @@
+package com.example.formatter;
+
+public class SampleRunner
+{
+
+	public static void main( String[] args )
+	{
+		Formatter formatter = new Formatter();
+		System.out.println( formatter.convertString( "[PM] 0:00.000" ));
+	}
+
+}
